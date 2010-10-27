edad38b3926238250a3ec6599dc573ead5dc617c
Matthew Scott <gldnspud@gmail.com>
2010-10-27 15:07:48 -0700

    Initial model. Would be nice to separate out roles so they are more customizable, but this fits our needs for now.

--- teams/models.py
+++ teams/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+from django.db import models as m
 
-# Create your models here.
+from treebeard.ns_tree import NS_Node
+
+
+class Team(NS_Node):
+    """A team of users."""
+
+    name = m.CharField(max_length=40)
+    slug = m.CharField(max_length=20)
+    members = m.ManyToManyField('auth.User', related_name='teams_member_of')
+    managers = m.ManyToManyField('auth.User', related_name='teams_manager_of')
