c389d34bea5e9fd03239b4a9d481af7a5f1be795
Matthew Scott <gldnspud@gmail.com>
2010-10-27 17:01:10 -0700

    Add a 'fullslug' property to Team.

--- teams/models.py
+++ teams/models.py
@@ -10,3 +10,14 @@ class Team(NS_Node):
     slug = m.CharField(max_length=20)
     members = m.ManyToManyField('auth.User', related_name='teams_member_of')
     managers = m.ManyToManyField('auth.User', related_name='teams_manager_of')
+
+    node_order_by = ['slug']
+
+    @property
+    def fullslug(self):
+        teams = list(self.get_ancestors())
+        teams.append(self)
+        return '.'.join(team.slug for team in teams)
+
+    def __unicode__(self):
+        return self.name
--- teams/tests.py
+++ teams/tests.py
@@ -1,23 +1,56 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
-"""
+"""Tests for django-teams"""
 
 from django.test import TestCase
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+from teams.models import Team
+
+
+class TeamsTest(TestCase):
+
+    def get(self, node):
+        return Team.objects.get(pk=node.id)
+
+    def setUp(self):
+        self.createTestTeams()
+
+    def createTestTeams(self):
+        self.a = Team.add_root(name='A', slug='a')
+        self.b = Team.add_root(name='B', slug='b')
+        self.aa = self.get(self.a).add_child(name='AA', slug='a')
+        self.ab = self.get(self.a).add_child(name='AB', slug='b')
+        self.aaa = self.get(self.aa).add_child(name='AAA', slug='a')
+        self.aba = self.get(self.ab).add_child(name='ABA', slug='a')
+        self.ba = self.get(self.b).add_child(name='BA', slug='a')
+
+    def test_fullslug_shows_dotted_hierarchy_of_slugs(self):
+        self.assertEqual('a', self.get(self.a).fullslug)
+        self.assertEqual('b', self.get(self.b).fullslug)
+        self.assertEqual('a.a', self.get(self.aa).fullslug)
+        self.assertEqual('a.b', self.get(self.ab).fullslug)
+        self.assertEqual('a.a.a', self.get(self.aaa).fullslug)
+        self.assertEqual('a.b.a', self.get(self.aba).fullslug)
+        self.assertEqual('b', self.get(self.b).fullslug)
+
+    def test_retrieve_team_by_fullslug(self):
         """
-        Tests that 1 + 1 always equals 2.
         """
-        self.failUnlessEqual(1 + 1, 2)
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+    def test_member_is_defacto_member_of_direct_ancestors(self):
+        """
+        """
 
->>> 1 + 1 == 2
-True
-"""}
+    def test_member_is_defacto_member_of_all_descendants(self):
+        """
+        """
 
+    def test_member_is_not_defacto_member_of_descendants_of_direct_ancestors(self):
+        """
+        """
+
+    def test_manager_is_defacto_manager_of_all_descendants(self):
+        """
+        """
+
+    def test_manager_is_not_defacto_manager_of_ancestors(self):
+        """
+        """
