18782ca1459f87aa8a3fe9bb45a83823194d117f
Matthew Scott <gldnspud@gmail.com>
2010-10-27 20:13:24 -0700

    Add has_manager and has_member methods to Team; add manager as member if needed; remove manager when removing member as needed

--- teams/models.py
+++ teams/models.py
@@ -1,4 +1,5 @@
 from django.db import models as m
+from django.db.models.signals import m2m_changed
 
 from treebeard.ns_tree import NS_Node
 
@@ -17,10 +18,40 @@ class Team(NS_Node):
     def __unicode__(self):
         return self.fullslug
 
+    def has_manager(self, user):
+        return Team.objects.filter(managers__in=[user]).count() > 0
+
+    def has_member(self, user):
+        return Team.objects.filter(members__in=[user]).count() > 0
+
     def save(self, *args, **kwargs):
         # Build fullslug if not specified.
         if self.fullslug == '':
             teams = list(self.get_ancestors())
             teams.append(self)
             self.fullslug = '.'.join(team.slug for team in teams)
+        #
+        # Continue saving.
         super(Team, self).save(*args, **kwargs)
+
+    @staticmethod
+    def on_members_changed(sender, instance, action, model, pk_set, **kwargs):
+        if action == 'post_remove':
+            team = instance
+            # For each member removed, also remove them as a manager if they
+            # are a manager.
+            leftover_managers = team.managers.filter(pk__in=pk_set)
+            team.managers.remove(*leftover_managers.all())
+
+    @staticmethod
+    def on_managers_changed(sender, instance, action, model, pk_set, **kwargs):
+        if action == 'post_add':
+            team = instance
+            # For each manager added, also add them as a member if they are
+            # not already.
+            non_members = team.managers.exclude(teams_member_of__in=[team])
+            team.members.add(*non_members.all())
+
+
+m2m_changed.connect(Team.on_members_changed, sender=Team.members.through)
+m2m_changed.connect(Team.on_managers_changed, sender=Team.managers.through)
--- teams/tests.py
+++ teams/tests.py
@@ -1,5 +1,6 @@
 """Tests for django-teams"""
 
+from django.contrib.auth.models import User
 from django.test import TestCase
 
 from teams.models import Team
@@ -10,8 +11,12 @@ class TeamsTest(TestCase):
     def get(self, node):
         return Team.objects.get(pk=node.id)
 
+    def team(self, fullslug):
+        return Team.objects.get(fullslug=fullslug)
+
     def setUp(self):
         self.createTestTeams()
+        self.createTestUser()
 
     def createTestTeams(self):
         self.a = Team.add_root(name='A', slug='a')
@@ -21,6 +26,9 @@ class TeamsTest(TestCase):
         self.aaa = self.get(self.aa).add_child(name='AAA', slug='a')
         self.aba = self.get(self.ab).add_child(name='ABA', slug='a')
         self.ba = self.get(self.b).add_child(name='BA', slug='a')
+
+    def createTestUser(self):
+        self.user = User.objects.create(username='user1')
 
     def test_fullslug_shows_dotted_hierarchy_of_slugs(self):
         self.assertEqual('a', self.get(self.a).fullslug)
@@ -39,6 +47,38 @@ class TeamsTest(TestCase):
         self.assertEqual(self.aaa, Team.objects.get(fullslug='a.a.a'))
         self.assertEqual(self.aba, Team.objects.get(fullslug='a.b.a'))
         self.assertEqual(self.b, Team.objects.get(fullslug='b'))
+
+    def test_user_is_member_of_team(self):
+        a = self.team('a')
+        a.members.add(self.user)
+        a.save()
+        self.assertTrue(a.has_member(self.user))
+        self.assertFalse(a.has_manager(self.user))
+
+    def test_user_is_manager_of_team(self):
+        a = self.team('a')
+        a.members.add(self.user)
+        a.managers.add(self.user)
+        a.save()
+        self.assertTrue(a.has_member(self.user))
+        self.assertTrue(a.has_manager(self.user))
+
+    def test_user_is_manager_of_team_and_automatically_a_member(self):
+        a = self.team('a')
+        a.managers.add(self.user)
+        a.save()
+        self.assertTrue(a.has_member(self.user))
+        self.assertTrue(a.has_manager(self.user))
+
+    def test_user_removed_from_team_no_longer_manager(self):
+        a = self.team('a')
+        a.managers.add(self.user)
+        a.save()
+        self.assertTrue(a.has_member(self.user))
+        self.assertTrue(a.has_manager(self.user))
+        a.members.remove(self.user)
+        self.assertFalse(a.has_member(self.user))
+        self.assertFalse(a.has_manager(self.user))
 
     def test_member_is_defacto_member_of_direct_ancestors(self):
         """
