86c86feebbd7e1e7b560d2e059aa94cd20b1d8f6
Matthew Scott <gldnspud@gmail.com>
2010-11-27 21:48:50 -0800

    Add Member model, and auto-join logic.

--- teams/models.py
+++ teams/models.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
 
 
 class Team(models.Model):
@@ -13,3 +15,24 @@ class Team(models.Model):
 
     def __unicode__(self):
         return self.name
+
+    def user_is_member(self, user):
+        return (Member.objects.filter(team=self, user=user).count() == 1)
+
+
+class Member(models.Model):
+
+    user = models.ForeignKey(User, related_name='team_members')
+    team = models.ForeignKey(Team, related_name='members')
+    is_coordinator = models.BooleanField(default=False)
+    joined = models.DateTimeField(auto_now_add=True)
+
+
+def auto_join_user_with_no_memberships(sender, instance=None, **kwargs):
+    if instance is not None:
+        membership_count = Member.objects.filter(user=instance).count()
+        if membership_count == 0:
+            for team in Team.objects.filter(is_private=False, auto_join=True):
+                Member.objects.create(user=instance, team=team)
+
+post_save.connect(auto_join_user_with_no_memberships, sender=User)
--- teams/tests.py
+++ teams/tests.py
@@ -1,15 +1,23 @@
 """Tests for django-teams"""
 
+from django.contrib.auth.models import User
 from django.test import TestCase
+
+from teams.models import Team, Member
 
 
 class TestTeams(TestCase):
 
-    def setUp(self):
-        pass
+    def test_auto_join(self):
+        team_auto = Team.objects.create(slug='auto', name='Auto Join', auto_join=True)
+        team_noauto = Team.objects.create(slug='noauto', name='No Auto Join')
+        user1 = User.objects.create(username='user1')
+        assert team_auto.user_is_member(user1)
+        assert not team_noauto.user_is_member(user1)
 
-    def tearDown(self):
-        pass
-
-    def test_noop(self):
-        pass
+    def test_private_no_auto_join(self):
+        team_auto = Team.objects.create(slug='auto', name='Auto Join', auto_join=True, is_private=True)
+        team_noauto = Team.objects.create(slug='noauto', name='No Auto Join', is_private=True)
+        user1 = User.objects.create(username='user1')
+        assert not team_auto.user_is_member(user1)
+        assert not team_noauto.user_is_member(user1)
