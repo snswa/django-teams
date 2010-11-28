d3bd29fb39c0d0851c80e22042b2790c25d352c4
Matthew Scott <gldnspud@gmail.com>
2010-11-27 18:31:07 -0800

    Simplify admin to go along with simplified models.

--- teams/admin.py
+++ teams/admin.py
@@ -1,21 +1,6 @@
 from django.contrib import admin
 
-from treebeard.admin import TreeAdmin
-
-from teams.models import Role, RoleMembership, Team
+from teams.models import Team
 
 
-class MembershipInline(admin.TabularInline):
-
-    model = RoleMembership
-
-
-class TeamAdmin(TreeAdmin):
-
-    inlines = [
-        MembershipInline,
-    ]
-
-
-admin.site.register(Role)
-admin.site.register(Team, TeamAdmin)
+admin.site.register(Team)
