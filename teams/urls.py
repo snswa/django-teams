3c0a732e2ce96e22497da7d9d3b74d59653a2866
Matthew Scott <gldnspud@gmail.com>
2010-11-29 01:07:05 -0800

    membership view for joining/leaving a team

--- teams/urls.py
+++ teams/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns(
     'teams.views',
     url(r'^$', views.index, name='teams_index'),
     url(r'^(?P<slug>[\w\._-]+)/$', views.team, name='teams_team'),
+    url(r'^(?P<slug>[\w\._-]+)/membership$', views.membership, name='teams_membership'),
 )
--- teams/views.py
+++ teams/views.py
@@ -1,12 +1,15 @@
 from operator import attrgetter
 
-from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
 from django.db.models import Q
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template.context import RequestContext
 
+from django.contrib.auth.decorators import login_required
 
-from teams.models import Team
+
+from teams.models import Member, Team
 
 
 @login_required
@@ -33,3 +36,18 @@ def team(request, slug):
     }
     return render_to_response(
         template_name, template_context, RequestContext(request))
+
+
+# @@@ needs test
+@login_required
+def membership(request, slug):
+    team = get_object_or_404(Team, slug=slug)
+    if request.method == 'POST':
+        if request.POST.get('join'):
+            print 'joining'
+            if not team.is_private and not request.user in team.members.all():
+                Member(team=team, user=request.user).save()
+        elif request.POST.get('leave'):
+            Member.objects.filter(team=team, user=request.user).delete()
+    redirect_to = reverse('teams_team', kwargs={'slug': slug})
+    return HttpResponseRedirect(redirect_to)
